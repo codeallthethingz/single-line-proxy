88052fe1936212f3d6680538e28e91740320c212
codeallthethingz <howdoicontactwill@gmail.com>
2020-07-15 16:16:23 -0700

    added port

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
-#!/usr/bin/env node 
+#!/usr/bin/env node
 const http = require('http')
 const simpleProxy = require('node-simple-proxy')
 const chalk = require('chalk')
+const { exception } = require('console')
 let port = 8000
 
 let args = process.argv.slice(2)
 
 let target = {}
 for (arg of args) {
-    let pathPort = arg.split(">")
-    console.log(`mapping ${chalk.blue(pathPort[0])} -> ${chalk.blueBright('localhost:'+pathPort[1])}`)
-    target[pathPort[0]] = `localhost:${pathPort[1]}`
+    try {
+        port = parseInt(arg)
+    } catch (exception) {
+        let pathPort = arg.split(">")
+        console.log(`mapping ${chalk.blue(pathPort[0])} -> ${chalk.blueBright('localhost:' + pathPort[1])}`)
+        target[pathPort[0]] = `localhost:${pathPort[1]}`
+    }
 }
 
 console.log(`listening on: ${chalk.green(port)}`)
--- readme.md
+++ readme.md
@@ -5,14 +5,14 @@ Command line utility to create a command line proxy.
 ## Usage
 
 ```
-npx single-line-proxy "/api/(.*)>3000" "/(.*)>3001"
+npx single-line-proxy "/api/(.*)>3000" "/(.*)>3001" 4000
 ```
 
-Will run a proxy on port 8000 and forward requests that match each of the path patterns to the corresponding port on localhost.
+Will run a proxy on port 4000 and forward requests that match each of the path patterns to the corresponding port on localhost.
 
 Or install it
 
 ```
 npm install -g single-line-proxy
-slp "/api/(.*)>3000" "/(.*)>3001"
+slp "/api/(.*)>3000" "/(.*)>3001" 4000
 ```
